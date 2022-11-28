fe22c899e2b6f9b5d726fb0f37031ede755a76ae
waiyan <wphyo8929@gmail.com>
2022-11-28 14:59:30 +0630

    form manipulation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import RegisterForm from './RegisterForm';
 
 
 
 function App() {
-  function handleClick() {
-    let randomNum = Math.floor(Math.random() *3) + 1;
-    console.log(randomNum);
-    let userInput = prompt('type a number');
-    alert(`Computer number: ${randomNum}, your guess: ${userInput}`);
-  }
+ 
   return (
     <div>
-      <h1>Task: Add a button and handle a click event</h1>
-      <button onClick={handleClick}>Guess the number between 1 and 3</button>
+      <RegisterForm />
     </div>
   )
   
