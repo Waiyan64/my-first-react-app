c559572791b517af3a1061a4a82459a7f2e3a690
waiyan <wphyo8929@gmail.com>
2022-11-28 13:05:17 +0630

    On to the Next experiment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Heading from './Heading';
-import ClickEvent from './ClickEvent';
+
 
 
 function App() {
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() *3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('type a number');
+    alert(`Computer number: ${randomNum}, your guess: ${userInput}`);
+  }
   return (
     <div>
-      <ClickEvent />
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>Guess the number between 1 and 3</button>
     </div>
   )
   
