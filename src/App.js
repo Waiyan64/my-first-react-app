ad53c6abd93704dfc748dacddb862d99de9a1e2a
waiyan <wphyo8929@gmail.com>
2022-12-03 11:16:20 +0630

    Audio

--- src/App.js
+++ src/App.js
@@ -2,28 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
-import Contact from './Contact';
-import Homepage from './Homepage';
-import AboutLittleLemon from './AboutLittleLemon';
+import ReactPlayer from "react-player/youtube";
 
 
 function App() {
+  const bird1 = new Audio( 
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3" 
+  ); 
+  const bird2 = new Audio( 
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3" 
+  ); 
  
-  return (
-    <div>
-      <nav>
-        <Link to="/" className='nav-item'>Homepage</Link>
-        <Link to="/about" className='nav-item'>About Little Lemon</Link>
-        <Link to="/contact" className='nav-item'>Contact</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/about" element={<AboutLittleLemon />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
-    </div>
-  );
-  
-};
+  function toggle1() { 
+    if (bird1.paused) { 
+      bird1.play(); 
+    } else { 
+      bird1.pause(); 
+    } 
+  }; 
+ 
+  function toggle2() { 
+    if (bird2.paused) { 
+      bird2.play(); 
+    } else { 
+      bird2.pause(); 
+    } 
+  }; 
+ 
+  return ( 
+    <div> 
+      <button onClick={toggle1}>Caspian Tern 1</button> 
+      <button onClick={toggle2}>Caspian Tern 2</button> 
+    </div> 
+  ); 
+} 
+ 
 
 export default App;
